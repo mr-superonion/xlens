e9d45c9814056acbdf7fb61c2db1f2847f83d2e0
Andy Park <chanhyup@andrew.cmu.edu>
2024-03-11 13:37:54 -0400

    Fix bug

--- xlens/simulation/simulator/__init__.py
+++ xlens/simulation/simulator/__init__.py
@@ -1,4 +1,4 @@
-from . import base, halo, loader
+from . import base, loader
 
 """
 simulator: for image simulation
@@ -7,4 +7,4 @@ summary: from catalog to average number
 neff: from catalog to effective galaxy number
 """
 
-__all__ = ["loader", "base", "halo"]
+__all__ = ["loader", "base"]
