5fed6067ac875f2b2142e608b05d6ff2711dc688
Zhou Conghao <zchusre@gmail.com>
2024-08-13 16:12:11 -0700

    add docstring for get_psf_array

--- xlens/simulation/measure/utils.py
+++ xlens/simulation/measure/utils.py
@@ -25,14 +25,27 @@ import numpy as np
 
 
 def get_psf_array(exposure, ngrid, psf_rcut=26, dg=250, gcent=None):
-    """This function returns the PSF model at the center of the exposure."""
+    """This function returns the mean PSF model in a grid of the exposure.
+
+    Args:
+        exposure (lsst.afw.image._exposure.ExposureF): input exposure to get the PSF
+        ngrid (int): size of the output grid
+        psf_rcut (int, optional): cutoff radius of psf in pixel. Defaults to 26.
+        dg (int, optional): grid spacing in pixel. Defaults to 250.
+        gcent (int, optional): center of the grid. Defaults to 0.5 * dg.
+
+    Returns:
+        ndarray: ngrid x ngrid array of the mean PSF
+    """
     if gcent is None:
         gcent = dg // 2
     bbox = exposure.getBBox()
     width, height = bbox.getWidth(), bbox.getHeight()
 
+    # make sure that width and height are multiples of dg
     width = (width // dg) * dg - 1
     height = (height // dg) * dg - 1
+
     # Calculate the central point
     x_array = np.arange(0, width, dg, dtype=int) + gcent
     y_array = np.arange(0, height, dg, dtype=int) + gcent
@@ -46,24 +59,44 @@ def get_psf_array(exposure, ngrid, psf_rcut=26, dg=250, gcent=None):
             centroid = lsst_geom.Point2I(xc, yc)
             data = exposure.getPsf().computeImage(centroid).getArray()
             dx = data.shape[0]
-            assert dx == data.shape[1]
+            assert (
+                dx == data.shape[1]
+            ), f"The PSF from exposure is not square: dx: {dx}, dy: {data.shape[1]}"
+
+            # if the size of the output grid is larger than the PSF, we need to shift the PSF
+            # to the center of the output grid
             if ngrid > dx:
                 shift = (ngrid - dx + 1) // 2
                 out[shift : shift + dx, shift : shift + dx] = (
                     out[shift : shift + dx, shift : shift + dx] + data
                 )
+            # if the size of the output grid is smaller than the PSF, we need to truncate the PSF
             else:
                 shift = -(ngrid - dx) // 2
                 out = out + data[shift : shift + ngrid, shift : shift + ngrid]
             ncount += 1
+    # take the average of the PSF in the grid
     out = out / ncount
     anacal.fpfs.base.truncate_square(out, psf_rcut)
     return out
 
 
 def get_gridpsf_obj(exposure, ngrid, psf_rcut=26, dg=250, gcent=None):
-    """This function returns the PSF model object at the center of the
-    exposure."""
+    """This function returns the mean PSF model object a grid of the exposure
+
+    Args:
+        exposure (lsst.afw.image._exposure.ExposureF): input exposure to get the PSF
+        ngrid (int): size of the output PSF dimension
+        psf_rcut (int, optional): cutoff radius of psf in pixel. Defaults to 26.
+        dg (int, optional): grid spacing in pixel. Defaults to 250.
+        gcent (int, optional): center of the grid. Defaults to 0.5 * dg.
+
+    Returns:
+        anacal.psf.GridPsf: PSF model object
+    """
+
+    # TODO: we can probably factor out the common code between get_psf_array and get_gridpsf_obj
+
     if gcent is None:
         gcent = dg // 2
     bbox = exposure.getBBox()
