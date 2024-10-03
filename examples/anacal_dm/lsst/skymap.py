a046d6f8b7fa3b1aa75cd093a382a4d67173ba79
Xiangchong Li <xiangchong.li@ipmu.jp>
2024-10-04 05:50:38 +0900

    add LSST examples

---
+++ examples/anacal_dm/lsst/skymap.py
@@ -0,0 +1,11 @@
+config.skyMap = "discrete"
+
+# Position of M31 (Andromeda)
+config.skyMap["discrete"].raList = [10.685]  # degrees
+config.skyMap["discrete"].decList = [41.269]  # degrees
+config.skyMap["discrete"].radiusList = [5.0]  # degrees
+config.skyMap["discrete"].pixelScale = 0.2  # LSST arcsec/pixel
+config.skyMap["discrete"].patchInnerDimensions = [5000, 5000]  # in pixels
+config.skyMap["discrete"].projection = "TAN"
+config.skyMap["discrete"].tractOverlap = 0
+config.name = "lsst_sim"
