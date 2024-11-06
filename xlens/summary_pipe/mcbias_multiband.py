fb50949aeebf3fd1022f7f7ab9c5e59ef29e56de
Andy Park <chanhyup@andrew.cmu.edu>
2024-11-06 10:46:04 -0500

    Clean code

--- xlens/summary_pipe/mcbias_multiband.py
+++ xlens/summary_pipe/mcbias_multiband.py
@@ -109,11 +109,11 @@ class McBiasMultibandPipeConfig(
         if len(self.connections.dataType) == 0:
             raise ValueError("connections.dataTape missing")
 
-        if self.shear_name not in ["g1", "g2"]:
+        if self.shear_name not in ["g1", "g2", "gt", "gx"]:
             raise FieldValidationError(
                 self.__class__.shear_name,
                 self,
-                "shear_name can only be 'g1' or 'g2'",
+                "shear_name can only be 'g1', 'g2', 'gt' or 'gx'",
             )
 
         if self.shape_name not in ["q1", "q2", "e1", "e2"]:
@@ -158,21 +158,18 @@ class McBiasMultibandPipe(PipelineTask):
         assert isinstance(self.config, McBiasMultibandPipeConfig)
         # Retrieve the filename of the input exposure
         inputs = butlerQC.get(inputRefs)
-        self.run(**inputs)
+        if self.sname[-1] in ["t", "x"]:
+            self.run_tx(**inputs)
+        else:
+            self.run_12(**inputs)
         return
 
-    def run(self, src00List, src01List, src10List, src11List):
+    def run_tx(self, src00List, src01List, src10List, src11List):
         en = self.ename
         egn = self.egname
         up1 = []
         up2 = []
         down = []
-        up1_t = []
-        up2_t = []
-        down_t = []
-        gt1 = []
-        gt2 = []
-        print(len(src00List))
         for src00, src01, src10, src11 in zip(
             src00List, src01List, src10List, src11List
         ):
@@ -189,10 +186,6 @@ class McBiasMultibandPipe(PipelineTask):
                 src10[egn] * src10["w"] + src10[en] * src10["w_g1"]
             ) + np.sum(src11[egn] * src11["w"] + src11[en] * src11["w_g1"])
 
-            up1.append(ep - em)
-            up2.append((em + ep) / 2.0)
-            down.append((rm + rp) / 2.0)
-
             theta_00 = np.arctan2(src00["y"], src00["x"])
             theta_01 = np.arctan2(src01["y"], src01["x"])
             theta_10 = np.arctan2(src10["y"], src10["x"])
@@ -203,28 +196,28 @@ class McBiasMultibandPipe(PipelineTask):
             et_10 = src10["e1"] * np.cos(2.0 * theta_10) + src10["e2"] * np.sin(2.0 * theta_10)
             et_11 = src11["e1"] * np.cos(2.0 * theta_11) + src11["e2"] * np.sin(2.0 * theta_11)
 
-            et_m = np.sum(et_00 * src00["w"]) + np.sum(et_01 * src01["w"])
-            et_p = np.sum(et_10 * src10["w"]) + np.sum(et_11 * src11["w"])
+            em = np.sum(et_00 * src00["w"]) + np.sum(et_01 * src01["w"])
+            ep = np.sum(et_10 * src10["w"]) + np.sum(et_11 * src11["w"])
 
             rm_00_1 = src00["e1_g1"] * src00["w"] + src00["e1"] * src00["w_g1"]
             rm_00_2 = src00["e2_g2"] * src00["w"] + src00["e2"] * src00["w_g2"]
-            rm_00 = np.sum(rm_00_1 * np.cos(2.0 * theta_00) + rm_00_2 * np.sin(2.0 * theta_00))
+            rm_00 = np.sum(rm_00_1 * np.cos(2.0 * theta_00)**2 + rm_00_2 * np.sin(2.0 * theta_00)**2)
 
             rm_01_1 = src01["e1_g1"] * src01["w"] + src01["e1"] * src01["w_g1"]
             rm_01_2 = src01["e2_g2"] * src01["w"] + src01["e2"] * src01["w_g2"]
-            rm_01 = np.sum(rm_01_1 * np.cos(2.0 * theta_01) + rm_01_2 * np.sin(2.0 * theta_01))
+            rm_01 = np.sum(rm_01_1 * np.cos(2.0 * theta_01)**2 + rm_01_2 * np.sin(2.0 * theta_01)**2)
 
-            rm_t = rm_00 + rm_01
+            rm = rm_00 + rm_01
 
             rp_10_1 = src10["e1_g1"] * src10["w"] + src10["e1"] * src10["w_g1"]
             rp_10_2 = src10["e2_g2"] * src10["w"] + src10["e2"] * src10["w_g2"]
-            rp_10 = np.sum(rp_10_1 * np.cos(2.0 * theta_10) + rp_10_2 * np.sin(2.0 * theta_10))
+            rp_10 = np.sum(rp_10_1 * np.cos(2.0 * theta_10)**2 + rp_10_2 * np.sin(2.0 * theta_10)**2)
 
             rp_11_1 = src11["e1_g1"] * src11["w"] + src11["e1"] * src11["w_g1"]
             rp_11_2 = src11["e2_g2"] * src11["w"] + src11["e2"] * src11["w_g2"]
-            rp_11 = np.sum(rp_11_1 * np.cos(2.0 * theta_11) + rp_11_2 * np.sin(2.0 * theta_11))
+            rp_11 = np.sum(rp_11_1 * np.cos(2.0 * theta_11)**2 + rp_11_2 * np.sin(2.0 * theta_11))**2
 
-            rp_t = rp_10 + rp_11
+            rp = rp_10 + rp_11
 
             up1_t.append(et_p - et_m)
             up2_t.append((et_m + et_p) / 2.0)
@@ -273,4 +266,65 @@ class McBiasMultibandPipe(PipelineTask):
                 )
         print("MC:",
               np.average(up1_t) / denom_t / 0.5 / 2.0 - 1)
+        return
+
+    def run_12(self, src00List, src01List, src10List, src11List):
+        en = self.ename
+        egn = self.egname
+        up1 = []
+        up2 = []
+        down = []
+        for src00, src01, src10, src11 in zip(
+            src00List, src01List, src10List, src11List
+        ):
+            src00 = src00.get()
+            src01 = src01.get()
+            src10 = src10.get()
+            src11 = src11.get()
+            em = np.sum(src00[en] * src00["w"]) + np.sum(src01[en] * src01["w"])
+            ep = np.sum(src10[en] * src10["w"]) + np.sum(src11[en] * src11["w"])
+            rm = np.sum(
+                src00[egn] * src00["w"] + src00[en] * src00["w_g1"]
+            ) + np.sum(src01[egn] * src01["w"] + src01[en] * src01["w_g1"])
+            rp = np.sum(
+                src10[egn] * src10["w"] + src10[en] * src10["w_g1"]
+            ) + np.sum(src11[egn] * src11["w"] + src11[en] * src11["w_g1"])
+
+            up1.append(ep - em)
+            up2.append((em + ep) / 2.0)
+            down.append((rm + rp) / 2.0)
+
+        nsim = len(src00List)
+        denom = np.average(down)
+        tmp = np.array(up1) / 2.0 + np.array(up2)
+        print(
+            "Positive shear:",
+            np.average(tmp) / denom,
+            "+-",
+            np.std(tmp) / denom / np.sqrt(nsim),
+        )
+        tmp = -np.array(up1) / 2.0 + np.array(up2)
+        print(
+            "Negative shear:",
+            np.average(tmp) / denom,
+            "+-",
+            np.std(tmp) / denom / np.sqrt(nsim),
+        )
+        if self.sname[-1] == self.ename[-1]:
+            print(
+                "Multiplicative bias:",
+                np.average(up1) / denom / self.svalue / 2.0 - 1,
+                "+-",
+                np.std(up1) / denom / np.sqrt(nsim) / self.svalue / 2.0,
+            )
+        else:
+            print(
+                "We do not estimate multiplicative bias:",
+            )
+        print(
+            "Additive bias:",
+            np.average(up2) / denom,
+            "+-",
+            np.std(up2) / denom / np.sqrt(nsim),
+        )
         return
