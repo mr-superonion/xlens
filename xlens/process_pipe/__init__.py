2038512de6f28a98819ab94ee60210134b66b983
Xiangchong Li <xiangchong.li@ipmu.jp>
2024-11-26 08:54:11 +0900

    add hsc's systematics function

--- xlens/process_pipe/__init__.py
+++ xlens/process_pipe/__init__.py
@@ -1,8 +1,11 @@
-from . import fpfs_force, fpfs_joint, fpfs_multiband, systematics_multiband
+from . import (
+    fpfs_force, fpfs_joint, fpfs_multiband, systematics_multiband, hsc
+)
 
 __all__ = [
     "fpfs_multiband",
     "systematics_multiband",
     "fpfs_joint",
     "fpfs_force",
+    "hsc",
 ]
