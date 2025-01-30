4f55ac3bd2af746617fb63492f9bbb8f44de7721
Andy Park <chanhyup@andrew.cmu.edu>
2025-01-30 11:21:09 -0500

    Add deep_anacal to measure

--- xlens/simulation/measure/__init__.py
+++ xlens/simulation/measure/__init__.py
@@ -1,9 +1,11 @@
 from . import utils
 from .anacal import ProcessSimAnacal
+from .deep_anacal import ProcessSimDeepAnacal
 from .base import ProcessSimDM
 
 __all__ = [
     "ProcessSimDM",
     "ProcessSimAnacal",
+    "ProcessSimDeepAnacal"
     "utils",
 ]
