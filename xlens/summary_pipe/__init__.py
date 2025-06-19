7c7f42c6bc111b860bf303ec9d1e6a8b4786f4f8
Xiangchong Li <mr.superonion@hotmail.com>
2025-06-20 02:23:06 +0900

    selbias test for fpfs_force

--- xlens/summary_pipe/__init__.py
+++ xlens/summary_pipe/__init__.py
@@ -1,14 +1,12 @@
 from . import (
     halo_mcbias_multiband,
     matchbias_multiband,
-    mcbias_multiband,
     neff,
     selbias_flux_multiband,
     selbias_ranforest_multiband,
 )
 
 __all__ = [
-    "mcbias_multiband",
     "matchbias_multiband",
     "halo_mcbias_multiband",
     "selbias_flux_multiband",
