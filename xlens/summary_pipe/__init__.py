95c995d181d99e0923cbc402c7722795cac8b741
Xiangchong Li <mr.superonion@hotmail.com>
2025-06-20 12:15:54 +0900

    delect matchbias

--- xlens/summary_pipe/__init__.py
+++ xlens/summary_pipe/__init__.py
@@ -1,13 +1,11 @@
 from . import (
     halo_mcbias_multiband,
-    matchbias_multiband,
     neff,
     selbias_flux_multiband,
     selbias_ranforest_multiband,
 )
 
 __all__ = [
-    "matchbias_multiband",
     "halo_mcbias_multiband",
     "selbias_flux_multiband",
     "selbias_ranforest_multiband",
