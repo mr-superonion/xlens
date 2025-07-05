dbc9c399b40e0bb54b77fdb5c570bc3fa4ef98b6
Xiangchong Li <mr.superonion@hotmail.com>
2025-07-05 11:15:24 +0900

    fix bug in redshift slices simulation

--- xlens/simulator/perturbation/zslice.py
+++ xlens/simulator/perturbation/zslice.py
@@ -1,30 +1,6 @@
 import galsim
 import numpy as np
-
-
-def _ternary(n: int, n_digits: int) -> str:
-    """Convert integer `n` to zero-padded base-3 string with `n_digits`
-    length."""
-    if n == 0:
-        return "0".zfill(n_digits)
-    digits = []
-    while n:
-        n, r = divmod(n, 3)
-        digits.append(str(r))
-    return ''.join(reversed(digits)).zfill(n_digits)
-
-
-def _get_shear_res_dict(
-    gso, lensed_shift, gamma1=-1, gamma2=-1, kappa=-1
-):
-    shear_res_dict = {
-        "gso": gso,
-        "lensed_shift": lensed_shift,
-        "gamma1": gamma1,
-        "gamma2": gamma2,
-        "kappa": kappa,
-    }
-    return shear_res_dict
+from .utils import _get_shear_res_dict, _ternary
 
 
 class ShearRedshift(object):
