17bc134aab42355b53a725464aef85e6eb79c508
Xiangchong Li <mr.superonion@hotmail.com>
2025-07-31 13:17:10 -0700

    remove unnecessary files

--- xlens/simulator/perturbation/__init__.py
+++ xlens/simulator/perturbation/__init__.py
@@ -1,7 +1,6 @@
 from . import utils
 from .dcr import DcrDistort
 from .halo import ShearHalo
-from .kappa import ShearKappa
 from .zslice import ShearRedshift
 
-__all__ = ["ShearHalo", "ShearKappa", "DcrDistort", "ShearRedshift", "utils"]
+__all__ = ["ShearHalo", "DcrDistort", "ShearRedshift", "utils"]
