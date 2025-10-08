d6b0fc0f4724e01263010365d7a1e2e8d8677ffd
Xiangchong Li <mr.superonion@hotmail.com>
2025-10-08 14:30:55 +0900

    fix bugs

--- xlens/process_pipe/anacal_force.py
+++ xlens/process_pipe/anacal_force.py
@@ -50,30 +50,7 @@ from numpy.typing import NDArray
 
 from ..processor.anacal import AnacalTask
 from ..processor.fpfs import FpfsMeasurementTask
-
-
-det_colnames = [
-    "ra", "dec", "flux", "dflux_dg1", "dflux_dg2", "dflux_dj1", "dflux_dj2",
-    "t", "dt_dg1", "dt_dg2", "dt_dj1", "dt_dj2",
-    "a1", "da1_dg1", "da1_dg2", "da1_dj1", "da1_dj2",
-    "a2", "da2_dg1", "da2_dg2", "da2_dj1", "da2_dj2",
-    "e1", "de1_dg1", "de1_dg2", "de1_dj1", "de1_dj2", "e2",
-    "de2_dg1", "de2_dg2", "de2_dj1", "de2_dj2",
-    "x1", "dx1_dg1", "dx1_dg2", "dx1_dj1", "dx1_dj2", "x2", "dx2_dg1",
-    "dx2_dg2", "dx2_dj1", "dx2_dj2",
-    "fluxap2", "dfluxap2_dg1", "dfluxap2_dg2", "dfluxap2_dj1", "dfluxap2_dj2",
-    "wdet", "dwdet_dg1", "dwdet_dg2", "dwdet_dj1", "dwdet_dj2",
-    "wsel", "dwsel_dg1", "dwsel_dg2", "dwsel_dj1", "dwsel_dj2",
-    "mask_value", "is_peak", "is_primary",
-    "fpfs_e1", "fpfs_de1_dg1", "fpfs_de1_dg2", "fpfs_de1_dj1", "fpfs_de1_dj2",
-    "fpfs_e2", "fpfs_de2_dg1", "fpfs_de2_dg2", "fpfs_de2_dj1", "fpfs_de2_dj2",
-    "fpfs_m0", "fpfs_dm0_dg1", "fpfs_dm0_dg2", "fpfs_dm0_dj1", "fpfs_dm0_dj2",
-    "fpfs_m2", "fpfs_dm2_dg1", "fpfs_dm2_dg2", "fpfs_dm2_dj1", "fpfs_dm2_dj2",
-    "peakv", "dpeakv_dg1", "dpeakv_dg2", "dpeakv_dj1", "dpeakv_dj2",
-    "bkg", "dbkg_dg1", "dbkg_dg2", "dbkg_dj1", "dbkg_dj2",
-    "x1_det", "x2_det",
-    "block_id",
-]
+import anacal
 
 
 class AnacalForcePipeConnections(
@@ -239,7 +216,9 @@ class AnacalForcePipe(PipelineTask):
         **kwargs,
     ):
         assert isinstance(self.config, AnacalForcePipeConfig)
-        detection = rfn.repack_fields(detection[det_colnames])
+        detection = rfn.repack_fields(
+            detection[list(anacal.table.column_names())]
+        )
         data = self.anacal.prepare_data(
             exposure=exposure,
             seed=seed,
