24245ff82ea70a78b0977216f3ef79328cc45aac
Xiangchong Li <12228372+mr-superonion@users.noreply.github.com>
2026-01-11 14:02:14 -0500

    Add systematics PSF star noise outputs

--- xlens/processor/build_systematics.py
+++ xlens/processor/build_systematics.py
@@ -2,12 +2,15 @@ from typing import Any
 
 import anacal
 import astropy.units as u
+import lsst.afw.image as afwImage
 import numpy as np
 from lsst.afw.image import ExposureF, MaskX
+from lsst.geom import Box2I, Extent2I, Point2D, Point2I
 from lsst.meas.algorithms import (
     LoadReferenceObjectsConfig,
     ReferenceObjectLoader,
 )
+from lsst.meas.base import SkyMapIdGeneratorConfig
 from lsst.pex.config import ConfigField, Field, ListField
 from lsst.pipe.base import (
     PipelineTask,
@@ -17,6 +20,7 @@ from lsst.pipe.base import (
 )
 from lsst.pipe.base import connectionTypes as cT
 import xlens
+from xlens.utils.image import resize_array, subpixel_shift
 
 
 class BuildSystematicsConnections(
@@ -43,6 +47,15 @@ class BuildSystematicsConnections(
         deferLoad=True,
         minimum=0,
     )
+    catalog = cT.Input(
+        doc="Catalog containing single-band measurement information.",
+        name="{coaddName}_coadd_meas",
+        storageClass="SourceCatalog",
+        dimensions=("skymap", "tract", "patch", "band"),
+        multiple=True,
+        deferLoad=True,
+        minimum=0,
+    )
     gaia = cT.PrerequisiteInput(
         doc="GAIA sources to load",
         name="gaia_dr3_20230707",
@@ -61,6 +74,24 @@ class BuildSystematicsConnections(
     outputPsf = cT.Output(
         doc="Stacked PSF array (6 x npix x npix).",
         name="deep_coadd_systematics_psfcentered",
+        storageClass="NumpyArray",
+        dimensions=("skymap", "tract", "patch"),
+    )
+    outputNoiseCorr = cT.Output(
+        doc="Stacked noise correlation array (6 x npix x npix).",
+        name="deep_coadd_systematics_noisecorr_stack",
+        storageClass="NumpyArray",
+        dimensions=("skymap", "tract", "patch"),
+    )
+    outputPsfCentered = cT.Output(
+        doc="Stacked PSF image array (6 x npix x npix).",
+        name="deep_coadd_systematics_psfcentered_stack",
+        storageClass="NumpyArray",
+        dimensions=("skymap", "tract", "patch"),
+    )
+    outputStarCentered = cT.Output(
+        doc="Stacked star image array (6 x npix x npix).",
+        name="deep_coadd_systematics_starcentered_stack",
         storageClass="NumpyArray",
         dimensions=("skymap", "tract", "patch"),
     )
@@ -86,6 +117,15 @@ class BuildSystematicsConfig(
         doc="Padding (pixels) when selecting GAIA sources around the patch.",
         default=300,
     )
+    psfCache = Field[int](
+        doc="Size of PSF cache",
+        default=100,
+    )
+    star_snr_min = Field[float](
+        doc="minimum (aperture) snr threshold of stars",
+        default=100.0,
+    )
+    idGenerator = SkyMapIdGeneratorConfig.make_field()
     gaiaLoader = ConfigField(
         dtype=LoadReferenceObjectsConfig,
         doc="Reference catalog loader",
@@ -116,6 +156,8 @@ class BuildSystematicsTask(PipelineTask):
             )
         else:
             gaia_loader = None
+        id_generator = self.config.idGenerator.apply(butlerQC.quantum.dataId)
+        seed = id_generator.catalog_id
         exposure_handles = inputs["exposure"]
         exposure_handles_dict = {
             handle.dataId["band"]: handle for handle in exposure_handles
@@ -126,10 +168,19 @@ class BuildSystematicsTask(PipelineTask):
             cell_handles_dict = None
         else:
             cell_handles_dict = {h.dataId["band"]: h for h in cell_handles}
+        catalog_handles = inputs["catalog"]
+        if len(catalog_handles) == 0:
+            catalog_handles_dict = None
+        else:
+            catalog_handles_dict = {
+                handle.dataId["band"]: handle for handle in catalog_handles
+            }
         outputs = self.run(
             exposure_handles_dict=exposure_handles_dict,
             gaia_loader=gaia_loader,
             cell_handles_dict=cell_handles_dict,
+            catalog_handles_dict=catalog_handles_dict,
+            seed=seed,
         )
         butlerQC.put(outputs, outputRefs)
         return
@@ -140,6 +191,8 @@ class BuildSystematicsTask(PipelineTask):
         exposure_handles_dict: dict[str, Any],
         gaia_loader: ReferenceObjectLoader | None = None,
         cell_handles_dict: None | dict[str, Any] = None,
+        catalog_handles_dict: None | dict[str, Any] = None,
+        seed: int | None = None,
         **kwargs,
     ) -> Struct:
         assert isinstance(self.config, BuildSystematicsConfig)
@@ -148,6 +201,12 @@ class BuildSystematicsTask(PipelineTask):
         template_wcs = None
         template_bbox = None
 
+        npix = self.config.npix
+        noise_corr_array = np.zeros((6, npix, npix))
+        psf_centered_array = np.zeros((6, npix, npix))
+        star_centered_array = np.zeros((6, npix, npix))
+
+        band_order = "ugrizy"
         for band, exp_handle in exposure_handles_dict.items():
             exp = exp_handle.get()
 
@@ -159,6 +218,27 @@ class BuildSystematicsTask(PipelineTask):
                 exposure=exp,
             )
             mask_array = self._merge_mask(mask_array, band_mask)
+
+            if band in band_order:
+                i = band_order.index(band)
+                noise_image = self.get_noise_corr(exp)
+                noise_corr_array[i] = noise_image.array
+
+                if (
+                    catalog_handles_dict is not None
+                    and band in catalog_handles_dict
+                ):
+                    catalog = catalog_handles_dict[band].get()
+                    psf_image, star_image = self.get_psf_systematics(
+                        exp,
+                        catalog,
+                        seed,
+                    )
+                    if psf_image is not None:
+                        psf_centered_array[i] = psf_image.array
+                    if star_image is not None:
+                        star_centered_array[i] = star_image.array
+                    del catalog
             del exp, band_mask
 
         if (
@@ -191,7 +271,6 @@ class BuildSystematicsTask(PipelineTask):
         )
 
         if cell_handles_dict is not None:
-            npix = self.config.npix
             psf_array = np.zeros((6, npix, npix))
             for i, band in enumerate("ugrizy"):
                 if band in cell_handles_dict.keys():
@@ -204,7 +283,13 @@ class BuildSystematicsTask(PipelineTask):
                     del cell_coadd
         else:
             psf_array = None
-        return Struct(outputMask=output_msk, outputPsf=psf_array)
+        return Struct(
+            outputMask=output_msk,
+            outputPsf=psf_array,
+            outputNoiseCorr=noise_corr_array,
+            outputPsfCentered=psf_centered_array,
+            outputStarCentered=star_centered_array,
+        )
 
     def _merge_mask(
         self, global_mask: np.ndarray | None, band_mask: np.ndarray,
@@ -255,3 +340,137 @@ class BuildSystematicsTask(PipelineTask):
         xy_r["y"] = y
         xy_r["r"] = r
         return xy_r
+
+    def get_noise_corr(self, exposure):
+        assert isinstance(self.config, BuildSystematicsConfig)
+        variance_array = exposure.getMaskedImage().variance.array[
+            1000:3000, 1000:3000
+        ]
+        window_array = (exposure.mask.array == 0).astype(np.float32)[
+            1000:3000, 1000:3000
+        ]
+
+        noise_array = np.asarray(
+            exposure.getMaskedImage().image.array,
+            dtype=np.float32,
+        )[1000:3000, 1000:3000]
+        window_array = (
+            window_array
+            * (noise_array**2.0 < variance_array * 9)
+            * (variance_array < 5.0)
+            * (~np.isnan(variance_array))
+        )
+
+        noise_array[~window_array.astype(bool)] = 0.0
+        noise_variance = np.average(variance_array[window_array.astype(bool)])
+        if noise_variance < 1e-20:
+            raise ValueError(
+                "the estimated image noise variance should be positive."
+            )
+
+        pad_width = ((10, 10), (10, 10))  # ((top, bottom), (left, right))
+        window_array = np.pad(
+            window_array,
+            pad_width=pad_width,
+            mode="constant",
+            constant_values=0.0,
+        )
+        noise_array = np.pad(
+            noise_array,
+            pad_width=pad_width,
+            mode="constant",
+            constant_values=0.0,
+        )
+        ny, nx = window_array.shape
+
+        npixl = int(self.config.npix // 2)
+        npixr = int(self.config.npix // 2 + 1)
+        noise_corr = np.fft.fftshift(
+            np.fft.ifft2(np.abs(np.fft.fft2(noise_array)) ** 2.0)
+        ).real[
+            ny // 2 - npixl : ny // 2 + npixr,
+            nx // 2 - npixl : nx // 2 + npixr,
+        ]
+        window_corr = np.fft.fftshift(
+            np.fft.ifft2(np.abs(np.fft.fft2(window_array)) ** 2.0)
+        ).real[
+            ny // 2 - npixl : ny // 2 + npixr,
+            nx // 2 - npixl : nx // 2 + npixr,
+        ]
+        noise_corr = noise_corr / window_corr
+        del window_array, noise_array, window_corr
+
+        noise_image = afwImage.ImageF(self.config.npix, self.config.npix)
+        noise_image.array[:, :] = noise_corr
+
+        return noise_image
+
+    def get_psf_systematics(self, exposure, catalog, seed):
+        assert isinstance(self.config, BuildSystematicsConfig)
+        if seed is None:
+            raise ValueError("Seed is required to select a random star.")
+        npixl = int(self.config.npix // 2)
+        npixr = int(self.config.npix // 2 + 1)
+
+        catalog = catalog.asAstropy().as_array()
+        msk = catalog["calib_psf_reserved"] & catalog["detect_isPrimary"]
+        catalog = catalog[msk]
+        snr = (
+            catalog["base_CircularApertureFlux_3_0_instFlux"]
+            / catalog["base_CircularApertureFlux_3_0_instFluxErr"]
+        )
+        bbox = exposure.getBBox()
+        xmin_exp, ymin_exp = bbox.getMinX(), bbox.getMinY()
+        xmax_exp, ymax_exp = bbox.getMaxX(), bbox.getMaxY()
+        msk2 = (
+            (catalog["base_SdssShape_x"] > xmin_exp + npixl)
+            & (catalog["base_SdssShape_y"] > ymin_exp + npixl)
+            & (catalog["base_SdssShape_x"] < xmax_exp - npixr)
+            & (catalog["base_SdssShape_y"] < ymax_exp - npixr)
+            & (snr > self.config.star_snr_min)
+        )
+        catalog = catalog[msk2]
+        nstars = len(catalog)
+
+        if nstars >= 1:
+            np.random.seed(seed)
+            ind = np.random.randint(0, nstars)
+            src = catalog[ind]
+
+            # Collect the PSF image
+            exposure.getPsf().setCacheCapacity(self.config.psfCache)
+            lsst_psf = exposure.getPsf()
+            psf_array = lsst_psf.computeImage(
+                Point2D(
+                    int(src["base_SdssShape_x"]),
+                    int(src["base_SdssShape_y"]),
+                )
+            ).getArray()
+            psf_array = resize_array(
+                psf_array,
+                (self.config.npix, self.config.npix),
+            )
+            psf_image = afwImage.ImageF(self.config.npix, self.config.npix)
+            psf_image.array[:, :] = psf_array
+
+            bbox = Box2I(
+                Point2I(
+                    int(src["base_SdssShape_x"]) - npixl,
+                    int(src["base_SdssShape_y"]) - npixl,
+                ),
+                Extent2I(self.config.npix, self.config.npix),
+            )
+
+            # Collect the star image
+            # Extract the sub-image using the BBox
+            star_image = exposure.Factory(exposure, bbox).getImage()
+            # Get the image component and convert to a NumPy array
+            star_array = star_image.getArray()
+            offset_x = src["base_SdssShape_x"] - int(src["base_SdssShape_x"])
+            offset_y = src["base_SdssShape_y"] - int(src["base_SdssShape_y"])
+            star_array = subpixel_shift(star_array, -offset_x, -offset_y)
+            star_image.array[:, :] = star_array
+        else:
+            psf_image = None
+            star_image = None
+        return psf_image, star_image
