b183aba92f6c6ed026ebb4666e809c86bf00b283
Xiangchong Li <xli6@bnl.gov>
2026-03-24 23:59:14 -0400

    change file name

--- xlens/processor/__init__.py
+++ xlens/processor/__init__.py
@@ -1,9 +1,9 @@
-from . import anacal, build_systematics, fpfs, measure_coadds, measure_sims
+from . import anacal, build_systematics, fpfs, measure_coadds, measure_coadds_sim
 
 __all__ = [
     "fpfs",
     "anacal",
     "build_systematics",
     "measure_coadds",
-    "measure_sims",
+    "measure_coadds_sim",
 ]
